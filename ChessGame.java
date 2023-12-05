61cbbf6bb1a14f22eb8dae6fdb07e90d6b63c1a8
Luncon <smitlucas2563@gmail.com>
2023-12-04 19:20:20 -0500

    Update ChessGame.java

--- ChessGame.java
+++ ChessGame.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 
 public class ChessGame extends JFrame {
     private JButton[][] boardButtons = new JButton[8][8];
-    private String[][] board = new String[8][8];
+    private Piece[][] board = new Piece[8][8];
     private boolean isPlayer1Turn = true;
     private JButton selectedButton = null;
 
@@ -14,15 +14,82 @@ public class ChessGame extends JFrame {
         initializeUI();
     }
 
-    private void initializeBoard() {
-        // Initialize the chess board with pieces in their initial positions
-        board[0] = new String[] { "R", "N", "B", "Q", "K", "B", "N", "R" };
-        board[1] = new String[] { "P", "P", "P", "P", "P", "P", "P", "P" };
-        for (int i = 2; i < 6; i++) {
-            board[i] = new String[] { "", "", "", "", "", "", "", "" };
+    private class Piece {
+        private String symbol;
+        private boolean isPlayer1;
+
+        public Piece(String symbol, boolean isPlayer1) {
+            this.symbol = symbol;
+            this.isPlayer1 = isPlayer1;
         }
-        board[6] = new String[] { "p", "p", "p", "p", "p", "p", "p", "p" };
-        board[7] = new String[] { "r", "n", "b", "q", "k", "b", "n", "r" };
+
+        public String getSymbol() {
+            return symbol;
+        }
+
+        public boolean isPlayer1() {
+            return isPlayer1;
+        }
+
+        public boolean isEmpty() {
+            if (this.symbol == "")
+                return true;
+            else
+                return false;
+        }
+    }
+
+    private void initializeBoard() {
+        board[0][0] = new Piece("R", true); // Rook
+        board[0][1] = new Piece("N", true); // Knight
+        board[0][2] = new Piece("B", true); // Bishop
+        board[0][3] = new Piece("Q", true); // Queen
+        board[0][4] = new Piece("K", true); // King
+        board[0][5] = new Piece("B", true); // Bishop
+        board[0][6] = new Piece("N", true); // Rook
+        board[0][7] = new Piece("R", true); // Knight
+        board[1][0] = new Piece("P", true);
+        board[1][1] = new Piece("P", true);
+        board[1][2] = new Piece("P", true);
+        board[1][3] = new Piece("P", true);
+        board[1][4] = new Piece("P", true); // Pawns
+        board[1][5] = new Piece("P", true);
+        board[1][6] = new Piece("P", true);
+        board[1][7] = new Piece("P", true);
+
+        for (int i = 2; i < 6; i++) {
+            for (int j = 0; j < 8; j++) {
+                board[i][j] = null;
+            }
+        }
+
+        board[7][0] = new Piece("r", true); // Rook
+        board[7][1] = new Piece("n", true); // Knight
+        board[7][2] = new Piece("b", true); // Bishop
+        board[7][3] = new Piece("q", true); // Queen
+        board[7][4] = new Piece("k", true); // King
+        board[7][5] = new Piece("b", true); // Bishop
+        board[7][6] = new Piece("n", true); // Rook
+        board[7][7] = new Piece("r", true); // Knight
+        board[6][0] = new Piece("p", true);
+        board[6][1] = new Piece("p", true);
+        board[6][2] = new Piece("p", true);
+        board[6][3] = new Piece("p", true);
+        board[6][4] = new Piece("p", true); // Pawns
+        board[6][5] = new Piece("p", true);
+        board[6][6] = new Piece("p", true);
+        board[6][7] = new Piece("p", true);
+
+        // Initialize the chess board with pieces in their initial positions
+        /*
+         * board[0] = new String[] { "R", "N", "B", "Q", "K", "B", "N", "R" };
+         * board[1] = new String[] { "P", "P", "P", "P", "P", "P", "P", "P" };
+         * for (int i = 2; i < 6; i++) {
+         * board[i] = new String[] { "", "", "", "", "", "", "", "" };
+         * }
+         * board[6] = new String[] { "p", "p", "p", "p", "p", "p", "p", "p" };
+         * board[7] = new String[] { "r", "n", "b", "q", "k", "b", "n", "r" };
+         */
     }
 
     private void initializeUI() {
@@ -34,7 +101,11 @@ public class ChessGame extends JFrame {
         // Create buttons for the chess board
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                boardButtons[i][j] = new JButton(board[i][j]);
+                if (board[i][j] != null) {
+                    boardButtons[i][j] = new JButton(board[i][j].getSymbol());
+                } else {
+                    boardButtons[i][j] = new JButton();
+                }
                 boardButtons[i][j].addActionListener(new ChessButtonListener(i, j));
                 add(boardButtons[i][j]);
             }
@@ -59,11 +130,12 @@ public class ChessGame extends JFrame {
             if (selectedButton == null) {
                 // If no piece is selected, highlight the button if it contains a piece of the
                 // current player
-                if (!board[row][col].isEmpty() && isPlayer1Turn && Character.isUpperCase(board[row][col].charAt(0))) {
+                if (!board[row][col].isEmpty() && isPlayer1Turn
+                        && Character.isUpperCase(board[row][col].getSymbol().charAt(0))) {
                     selectedButton = clickedButton;
                     selectedButton.setBackground(Color.YELLOW);
                 } else if (!board[row][col].isEmpty() && !isPlayer1Turn
-                        && Character.isLowerCase(board[row][col].charAt(0))) {
+                        && Character.isLowerCase(board[row][col].getSymbol().charAt(0))) {
                     selectedButton = clickedButton;
                     selectedButton.setBackground(Color.YELLOW);
                 }
@@ -76,8 +148,8 @@ public class ChessGame extends JFrame {
                 if (isValidMove(selectedRow, selectedCol, row, col)) {
                     // Move the piece to the new position
                     board[row][col] = board[selectedRow][selectedCol];
-                    board[selectedRow][selectedCol] = "";
-                    clickedButton.setText(board[row][col]);
+                    board[selectedRow][selectedCol] = null;
+                    clickedButton.setText(board[row][col].getSymbol());
 
                     selectedButton.setText("");
 
